e0e63564c6ec58f63676aef5e21f64e152fa2207
Naman Ghanekar <163579115+namanghanekar@users.noreply.github.com>
2026-04-14 16:09:46 +0530

    Add files via upload

---
+++ week-1/day-1/calculator.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b float64
+	var op string
+
+	fmt.Print("Enter calculation : ")
+
+	fmt.Scan(&a, &op, &b)// taking input from user
+
+	switch op {
+	case "+":
+		fmt.Println("Result:", a+b)
+	case "-":
+		fmt.Println("Result:", a-b)
+	case "*":
+		fmt.Println("Result:", a*b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Cannot divide by zero")
+			return
+		}
+		fmt.Println("Result:", a/b)
+	default:
+		fmt.Println("Invalid operator")
+	}
+}
